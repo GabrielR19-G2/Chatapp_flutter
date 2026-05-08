f82e4713a5047e4c3e56f1529944cd043c7428a4
Gabriel Ramos <gabrielfco02@hotmail.com>
2026-05-08 11:21:49 -0700

    upt: login page v1

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -7,7 +7,21 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffF2F2F2),
-      body: SafeArea(child: Column(children: [_Logo(), SizedBox(height: 15)])),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _Logo(),
+
+            _Form(),
+            _Labels(),
+            Text(
+              "Términos y condiciones de uso",
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -19,6 +33,7 @@ class _Logo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        margin: const EdgeInsets.only(top: 50),
         width: 170,
         child: Column(
           children: [
@@ -41,6 +56,51 @@ class _Form extends StatefulWidget {
 class __FormState extends State<_Form> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          TextField(),
+          TextField(),
+          ElevatedButton(
+            onPressed: () {},
+            child: Icon(Icons.join_inner_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            'No tienes una cuenta?',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 15,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          SizedBox(height: 10),
+          GestureDetector(
+            onTap: () {
+              print('navegacion pendiente crea una ahora.');
+            },
+            child: Text(
+              'Crea una ahora!',
+              style: TextStyle(
+                color: Colors.blue[600],
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
